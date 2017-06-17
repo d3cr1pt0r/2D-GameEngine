129ffab870929dce2f793d467cfb1aa6ee24ba01
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-06-17 16:12:31 +0200

    * interleaved vertex data

--- Engine/Renderable.cpp
+++ Engine/Renderable.cpp
@@ -3,6 +3,15 @@
 namespace Engine {
 
 	Renderable::Renderable() {
+
+	}
+
+	Renderable::Renderable(const Engine::Vector2f &position, const Engine::Vector2f &scale, const Color &color) :
+		transform_(position, scale, 0),
+		color_(color),
+		vbo_id_(0),
+		shader_("../Engine/Shaders/default.vert", "../Engine/Shaders/default.frag")
+	{
 	}
 
 	Renderable::~Renderable() {
@@ -12,15 +21,28 @@ namespace Engine {
 	void Renderable::init() {
 		glGenBuffers(1, &vbo_id_);
 		generateQuad(transform_.position_.x_, transform_.position_.y_, transform_.scale_.x_, transform_.scale_.y_);
+
+		shader_.init();
+		shader_.addAttribute("vertex_position");
+		shader_.addAttribute("vertex_color");
+	}
+
+	void Renderable::update() {
+		generateQuad(transform_.position_.x_, transform_.position_.y_, transform_.scale_.x_, transform_.scale_.y_);
 	}
 
 	void Renderable::render() {
+		shader_.bind();
+
 		glBindBuffer(GL_ARRAY_BUFFER, vbo_id_);
 		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position_));
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color_));
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDisableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		shader_.unbind();
 	}
 
 	void Renderable::destroy() {
@@ -28,23 +50,30 @@ namespace Engine {
 	}
 
 	void Renderable::generateQuad(float x, float y, float width, float height) {
-		vertex_data_[0] = x + width;
-		vertex_data_[1] = y + height;
+		vertex_data_[0].position_.x_ = x + width;
+		vertex_data_[0].position_.y_ = y + height;
 
-		vertex_data_[2] = x;
-		vertex_data_[3] = y + height;
+		vertex_data_[1].position_.x_ = x;
+		vertex_data_[1].position_.y_ = y + height;
 
-		vertex_data_[4] = x;
-		vertex_data_[5] = y;
+		vertex_data_[2].position_.x_ = x;
+		vertex_data_[2].position_.y_ = y;
 
-		vertex_data_[6] = x;
-		vertex_data_[7] = y;
+		vertex_data_[3].position_.x_ = x;
+		vertex_data_[3].position_.y_ = y;
 
-		vertex_data_[8] = x + width;
-		vertex_data_[9] = y;
+		vertex_data_[4].position_.x_ = x + width;
+		vertex_data_[4].position_.y_ = y;
 
-		vertex_data_[10] = x + width;
-		vertex_data_[11] = y + height;
+		vertex_data_[5].position_.x_ = x + width;
+		vertex_data_[5].position_.y_ = y + height;
+
+		for (int i = 0; i < 6; i++) {
+			vertex_data_[i].color_.r_ = color_.r_;
+			vertex_data_[i].color_.g_ = color_.g_;
+			vertex_data_[i].color_.b_ = color_.b_;
+			vertex_data_[i].color_.a_ = color_.a_;
+		}
 
 		glBindBuffer(GL_ARRAY_BUFFER, vbo_id_);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data_), vertex_data_, GL_STATIC_DRAW);
--- Engine/Renderable.h
+++ Engine/Renderable.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <glew.h>
 #include "Transform.h"
+#include "Color.h"
+#include "GLSLShader.h"
+#include "Vertex.h"
 
 namespace Engine {
 
@@ -8,21 +11,25 @@ namespace Engine {
 
 	public:
 		Renderable();
+		Renderable(const Engine::Vector2f &position, const Engine::Vector2f &scale, const Color &color);
 		~Renderable();
 
 		void init();
+		void update();
 		void render();
 		void destroy();
 
 	public:
 		Transform transform_;
+		Color color_;
 
 	private:
 		void generateQuad(float x, float y, float width, float height);
 
 	private:
 		GLuint vbo_id_ = 0;
-		float vertex_data_[12];
+		GLSLShader shader_;
+		Vertex vertex_data_[6];
 	};
 }
 

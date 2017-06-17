1744b69bc8f3b08dd6821dbb8f2f115c345e8b6f
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-06-17 14:43:30 +0200

    * hack to keep console open after crash

--- Asteroids/main.cpp
+++ Asteroids/main.cpp
@@ -4,11 +4,14 @@
 #include "AsteroidsGame.h"
 
 int main(int argc, char *argv[]) {
+	bool block_input;
+
 	AsteroidsGame *game = new AsteroidsGame();
 	Engine::CoreEngine core_engine(game, "SDL_ASTEROIDS", 800, 600, 60);
 
 	if (!core_engine.init()) {
 		std::cout << "Failed to initialize CoreEngine!" << std::endl;
+		std::cin >> block_input;
 		return EXIT_FAILURE;
 	}
 

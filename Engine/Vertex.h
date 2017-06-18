ed3962eefe740cf80711357f10c12777736f361b
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-06-18 19:31:07 +0200

    * added uv data

--- Engine/Vertex.h
+++ Engine/Vertex.h
@@ -1,19 +1,27 @@
 #pragma once
 #include <glew.h>
 
-struct Position {
+struct VertexVector {
 	float x_;
 	float y_;
+	float z_;
 };
 
-struct Color {
+struct VertexColor {
 	float r_;
 	float g_;
 	float b_;
 	float a_;
 };
 
+struct VertexUv {
+	float x_;
+	float y_;
+};
+
 struct Vertex {
-	Position position_;
-	Color color_;
+	VertexVector position_;
+	VertexVector normal_;
+	VertexUv uv_;
+	VertexColor color_;
 };

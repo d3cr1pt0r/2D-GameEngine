3ceda36666a18e8616e43933e59f8cd39eb50b8e
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-06-18 13:31:48 +0200

    * camera test

--- Asteroids/AsteroidsGame.cpp
+++ Asteroids/AsteroidsGame.cpp
@@ -17,7 +17,7 @@ AsteroidsGame::~AsteroidsGame() {
 void AsteroidsGame::init() {
 	camera_.setToOrtographic(Engine::Window::width_, Engine::Window::height_, 1.0f);
 
-	renderable_.transform_.setPosition(glm::vec3(200.0f, 200.0f, 0.0f));
+	renderable_.transform_.setPosition(glm::vec3(Engine::Window::width_ * 0.5f, Engine::Window::height_ * 0.5f, 0.0f));
 	renderable_.transform_.setScale(glm::vec3(5.0f, 5.0f, 1.0f));
 	renderable_.color_ = Engine::Color(0.0, 1.0, 0.0, 1.0);
 
@@ -32,6 +32,26 @@ void AsteroidsGame::update(const float &delta_time) {
 	}
 	if (Engine::Input::getKeyUp(SDL_SCANCODE_1)) {
 		drag_mode_ = false;
+	}
+
+	if (Engine::Input::getKey(SDL_SCANCODE_2)) {
+		camera_.transform_.move(glm::vec3(0.0f, 0.0f, delta_time * speed));
+	}
+	if (Engine::Input::getKey(SDL_SCANCODE_3)) {
+		camera_.transform_.move(glm::vec3(0.0f, 0.0f, -delta_time * speed));
+	}
+
+	if (Engine::Input::getKey(SDL_SCANCODE_UP)) {
+		camera_.transform_.move(glm::vec3(0.0f, delta_time * speed, 0.0f));
+	}
+	if (Engine::Input::getKey(SDL_SCANCODE_DOWN)) {
+		camera_.transform_.move(glm::vec3(0.0f, -delta_time * speed, 0.0f));
+	}
+	if (Engine::Input::getKey(SDL_SCANCODE_LEFT)) {
+		camera_.transform_.move(glm::vec3(-delta_time * speed, 0.0f, 0.0f));
+	}
+	if (Engine::Input::getKey(SDL_SCANCODE_RIGHT)) {
+		camera_.transform_.move(glm::vec3(delta_time * speed, 0.0f, 0.0f));
 	}
 
 	if (Engine::Input::getKey(SDL_SCANCODE_W)) {

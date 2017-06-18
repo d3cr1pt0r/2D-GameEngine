56cf6f6bbdf3c71bd0ddc538ecb132655e76bec5
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-06-18 23:36:05 +0200

    * testing transforms

--- Asteroids/AsteroidsGame.cpp
+++ Asteroids/AsteroidsGame.cpp
@@ -8,21 +8,22 @@
 #include <Engine/Window.h>
 #include <Engine/Log.h>
 
-AsteroidsGame::AsteroidsGame() : camera_(), sprite_() {
-}
-
-AsteroidsGame::~AsteroidsGame() {
-}
-
 void AsteroidsGame::init() {
 	camera_.setToOrtographic(Engine::Window::width_, Engine::Window::height_, -1.0f, 100.0f, 1.0f);
 	camera_.clear_color_ = Engine::Color(0.1f, 0.1f, 0.1f, 1.0f);
 
 	sprite_.transform_.setPosition(glm::vec3(Engine::Window::width_ * 0.5f, Engine::Window::height_ * 0.5f, 0.0f));
-	sprite_.transform_.setScale(glm::vec3(5.0f, 5.0f, 1.0f));
+	sprite_.transform_.setScale(glm::vec3(5.0f, 5.0f, 5.0f));
 	sprite_.color_ = Engine::Color(0.0, 1.0, 0.0, 1.0);
 
+	sprite2_.transform_.setPosition(glm::vec3(10.0f, 10.0f, 0.0f));
+	sprite2_.transform_.setScale(glm::vec3(1.0f, 1.0f, 1.0f));
+	sprite2_.color_ = Engine::Color(0.0, 0.0, 1.0, 1.0);
+
 	sprite_.init();
+	sprite2_.init();
+
+	sprite_.transform_.addChild(&sprite2_.transform_);
 }
 
 void AsteroidsGame::update(const float &delta_time) {
@@ -33,26 +34,6 @@ void AsteroidsGame::update(const float &delta_time) {
 	}
 	if (Engine::Input::getKeyUp(SDL_SCANCODE_1)) {
 		drag_mode_ = false;
-	}
-
-	if (Engine::Input::getKey(SDL_SCANCODE_2)) {
-		camera_.transform_.move(glm::vec3(0.0f, 0.0f, delta_time * speed));
-	}
-	if (Engine::Input::getKey(SDL_SCANCODE_3)) {
-		camera_.transform_.move(glm::vec3(0.0f, 0.0f, -delta_time * speed));
-	}
-
-	if (Engine::Input::getKey(SDL_SCANCODE_UP)) {
-		camera_.transform_.move(glm::vec3(0.0f, delta_time * speed, 0.0f));
-	}
-	if (Engine::Input::getKey(SDL_SCANCODE_DOWN)) {
-		camera_.transform_.move(glm::vec3(0.0f, -delta_time * speed, 0.0f));
-	}
-	if (Engine::Input::getKey(SDL_SCANCODE_LEFT)) {
-		camera_.transform_.move(glm::vec3(-delta_time * speed, 0.0f, 0.0f));
-	}
-	if (Engine::Input::getKey(SDL_SCANCODE_RIGHT)) {
-		camera_.transform_.move(glm::vec3(delta_time * speed, 0.0f, 0.0f));
 	}
 
 	if (Engine::Input::getKey(SDL_SCANCODE_W)) {
@@ -74,6 +55,13 @@ void AsteroidsGame::update(const float &delta_time) {
 		sprite_.transform_.rotate(glm::vec3(0.0f, 0.0f, delta_time));
 	}
 
+	if (Engine::Input::getKey(SDL_SCANCODE_2)) {
+		sprite_.transform_.scale(glm::vec3(-delta_time * 10.0f, -delta_time * 10.0f, 0.0f));
+	}
+	if (Engine::Input::getKey(SDL_SCANCODE_3)) {
+		sprite_.transform_.scale(glm::vec3(delta_time * 10.0f, delta_time * 10.0f, 0.0f));
+	}
+
 	if (drag_mode_) {
 		glm::vec3 mouse_position = glm::vec3(Engine::Input::getMousePosition().x, Engine::Input::getMousePosition().y, 0.0f);
 		sprite_.transform_.setPosition(mouse_position - sprite_.transform_.getScale() * 0.5f);
@@ -82,8 +70,10 @@ void AsteroidsGame::update(const float &delta_time) {
 
 void AsteroidsGame::render() {
 	sprite_.render();
+	sprite2_.render();
 }
 
 void AsteroidsGame::destroy() {
 	sprite_.destroy();
+	sprite2_.destroy();
 }
--- Asteroids/AsteroidsGame.h
+++ Asteroids/AsteroidsGame.h
@@ -6,9 +6,6 @@
 class AsteroidsGame : public Engine::Game {
 
 public:
-	AsteroidsGame();
-	~AsteroidsGame();
-
 	void init() override;
 	void update(const float &delta_time) override;
 	void render() override;
@@ -17,6 +14,7 @@ public:
 private:
 	Engine::Camera camera_;
 	Engine::Sprite sprite_;
+	Engine::Sprite sprite2_;
 	bool drag_mode_ = false;
 
 };

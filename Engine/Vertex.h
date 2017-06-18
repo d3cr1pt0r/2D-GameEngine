3dba83cb687acca15f46636dd352b0067334f860
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-06-18 13:15:41 +0200

    * moved structs

--- Engine/Vertex.h
+++ Engine/Vertex.h
@@ -1,16 +1,19 @@
 #pragma once
 #include <glew.h>
 
-struct Vertex {
-	struct Position {
-		float x_;
-		float y_;
-	} position_;
+struct Position {
+	float x_;
+	float y_;
+};
 
-	struct Color {
-		float r_;
-		float g_;
-		float b_;
-		float a_;
-	} color_;
+struct Color {
+	float r_;
+	float g_;
+	float b_;
+	float a_;
+};
+
+struct Vertex {
+	Position position_;
+	Color color_;
 };

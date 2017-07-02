4f4309905918c9b4261275dbf6e85f786cc4a0cf
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-07-02 14:47:32 +0200

    * added ObjectManager and RenderManager

--- Engine/Manager.cpp
+++ Engine/Manager.cpp
@@ -15,22 +15,30 @@ namespace Engine {
 			instance_->log_manager_ = new LogManager();
 			instance_->window_manager_ = new WindowManager();
 			instance_->input_manager_ = new InputManager();
+			instance_->object_manager_ = new ObjectManager();
 			instance_->camera_manager_ = new CameraManager();
+			instance_->render_manager_ = new RenderManager();
 
 			instance_->log_manager_->init();
 			instance_->window_manager_->init();
 			instance_->input_manager_->init();
+			instance_->object_manager_->init();
 			instance_->camera_manager_->init();
+			instance_->render_manager_->init();
 		}
 	}
 
 	void Manager::destroy() {
+		instance_->render_manager_->destroy();
 		instance_->camera_manager_->destroy();
+		instance_->object_manager_->destroy();
 		instance_->input_manager_->destroy();
 		instance_->window_manager_->destroy();
 		instance_->log_manager_->destroy();
 
+		delete instance_->render_manager_;
 		delete instance_->camera_manager_;
+		delete instance_->object_manager_;
 		delete instance_->input_manager_;
 		delete instance_->window_manager_;
 		delete instance_->log_manager_;
--- Engine/Manager.h
+++ Engine/Manager.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "WindowManager.h"
 #include "InputManager.h"
+#include "ObjectManager.h"
 #include "CameraManager.h"
+#include "RenderManager.h"
 #include "LogManager.h"
 
 #define pManager Manager::getInstance()
-#define pLogManager pManager->log_manager_
 #define pWindowManager pManager->window_manager_
 #define pInputManager pManager->input_manager_
+#define pObjectManager pManager->object_manager_
 #define pCameraManager pManager->camera_manager_
+#define pRenderManager pManager->render_manager_
+#define pLogManager pManager->log_manager_
 
 namespace Engine {
 
@@ -22,7 +26,9 @@ namespace Engine {
 	public:
 		WindowManager *window_manager_;
 		InputManager *input_manager_;
+		ObjectManager *object_manager_;
 		CameraManager *camera_manager_;
+		RenderManager *render_manager_;
 		LogManager *log_manager_;
 
 	private:

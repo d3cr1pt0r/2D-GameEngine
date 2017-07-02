b0570136591b3d0c78b2801e2e5dad4e802620db
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-07-02 14:46:57 +0200

    * added ObjectManager for handling all present objects and components

---
+++ Engine/ObjectManager.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "BaseManager.h"
+#include "Object.h"
+#include "Component.h"
+#include "RenderableComponent.h"
+
+#include <vector>
+#include <unordered_map>
+
+#define MAX_OBJECTS		4294967296
+
+namespace Engine {
+
+	class ObjectManager : public BaseManager {
+
+	public:
+		void init() override;
+		void destroy() override;
+
+		void addObject(Object *object);
+		void removeObject(const Object *object);
+
+		void addComponent(Component *component);
+		void removeComponent(Component *component);
+
+		void addRenderableComponent(RenderableComponent *renderable_component);
+		void removeRenderableComponent(RenderableComponent *renderable_component);
+
+		std::unordered_map<unsigned int, Object*>& getObjects();
+		std::unordered_map<unsigned int, Component*>& getComponents();
+		std::unordered_map<unsigned int, RenderableComponent*>& getRenderableComponents();
+
+	private:
+		std::unordered_map<unsigned int, Object*> objects_;
+		std::unordered_map<unsigned int, Component*> components_;
+		std::unordered_map<unsigned int, RenderableComponent*> renderable_components_;
+	};
+}

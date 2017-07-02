4d431a24824095ace5684aaafc9e4d5b47a15347
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-07-02 14:48:36 +0200

    * test

--- Asteroids/AsteroidsGame.cpp
+++ Asteroids/AsteroidsGame.cpp
@@ -4,23 +4,32 @@
 #include <glm.hpp>
 
 #include <Engine/Manager.h>
+#include <Engine/GameObject.h>
+#include <Engine/SpriteRenderer.h>
+#include <Engine/Color.h>
 
 void AsteroidsGame::init() {
 	int width = Engine::pWindowManager->getWidth();
 	int height = Engine::pWindowManager->getHeight();
 
-	sprite_.transform_.setPosition(glm::vec3(width * 0.5f, height * 0.5f, 0.0f));
-	sprite_.transform_.setScale(glm::vec3(5.0f, 5.0f, 5.0f));
-	sprite_.color_ = Engine::Color(0.0, 1.0, 0.0, 1.0);
+	game_object_1_ = new Engine::GameObject("GameObject 1");
+	game_object_2_ = new Engine::GameObject("GameObject 2");
 
-	sprite2_.transform_.setPosition(glm::vec3(10.0f, 10.0f, 0.0f));
-	sprite2_.transform_.setScale(glm::vec3(1.0f, 1.0f, 1.0f));
-	sprite2_.color_ = Engine::Color(0.0, 0.0, 1.0, 1.0);
+	sprite_renderer_1 = new Engine::SpriteRenderer(Engine::Color(0.0f, 1.0f, 0.0f, 1.0f));
+	sprite_renderer_2 = new Engine::SpriteRenderer(Engine::Color(0.0f, 0.0f, 1.0f, 1.0f));
 
-	sprite_.init();
-	sprite2_.init();
+	game_object_1_->addComponent(sprite_renderer_1);
+	game_object_2_->addComponent(sprite_renderer_2);
 
-	sprite_.transform_.addChild(&sprite2_.transform_);
+	game_object_1_->transform_.setPosition(glm::vec3(width * 0.5f, height * 0.5f, 0.0f));
+	game_object_1_->transform_.setScale(glm::vec3(5.0f, 5.0f, 5.0f));
+	sprite_renderer_1->color_ = Engine::Color(0.0, 1.0, 0.0, 1.0);
+
+	game_object_2_->transform_.setPosition(glm::vec3(10.0f, 10.0f, 0.0f));
+	game_object_2_->transform_.setScale(glm::vec3(1.0f, 1.0f, 1.0f));
+	sprite_renderer_1->color_ = Engine::Color(0.0, 0.0, 1.0, 1.0);
+
+	game_object_1_->transform_.addChild(&game_object_2_->transform_);
 }
 
 void AsteroidsGame::update(const float &delta_time) {
@@ -34,43 +43,41 @@ void AsteroidsGame::update(const float &delta_time) {
 	}
 
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_W)) {
-		sprite_.transform_.move(glm::vec3(0.0f, delta_time * speed, 0.0f));
+		game_object_1_->transform_.move(glm::vec3(0.0f, delta_time * speed, 0.0f));
 	}
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_S)) {
-		sprite_.transform_.move(glm::vec3(0.0f, -delta_time * speed, 0.0f));
+		game_object_1_->transform_.move(glm::vec3(0.0f, -delta_time * speed, 0.0f));
 	}
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_A)) {
-		sprite_.transform_.move(glm::vec3(-delta_time * speed, 0.0f, 0.0f));
+		game_object_1_->transform_.move(glm::vec3(-delta_time * speed, 0.0f, 0.0f));
 	}
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_D)) {
-		sprite_.transform_.move(glm::vec3(delta_time * speed, 0.0f, 0.0f));
+		game_object_1_->transform_.move(glm::vec3(delta_time * speed, 0.0f, 0.0f));
 	}
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_Q)) {
-		sprite_.transform_.rotate(glm::vec3(0.0f, 0.0f, -delta_time));
+		game_object_1_->transform_.rotate(glm::vec3(0.0f, 0.0f, -delta_time));
 	}
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_E)) {
-		sprite_.transform_.rotate(glm::vec3(0.0f, 0.0f, delta_time));
+		game_object_1_->transform_.rotate(glm::vec3(0.0f, 0.0f, delta_time));
 	}
 
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_2)) {
-		sprite_.transform_.scale(glm::vec3(-delta_time * 10.0f, -delta_time * 10.0f, 0.0f));
+		game_object_1_->transform_.scale(glm::vec3(-delta_time * 10.0f, -delta_time * 10.0f, 0.0f));
 	}
 	if (Engine::pInputManager->getKey(SDL_SCANCODE_3)) {
-		sprite_.transform_.scale(glm::vec3(delta_time * 10.0f, delta_time * 10.0f, 0.0f));
+		game_object_1_->transform_.scale(glm::vec3(delta_time * 10.0f, delta_time * 10.0f, 0.0f));
 	}
 
 	if (drag_mode_) {
 		glm::vec3 mouse_position = glm::vec3(Engine::pInputManager->getMousePosition().x, Engine::pInputManager->getMousePosition().y, 0.0f);
-		sprite_.transform_.setPosition(mouse_position - sprite_.transform_.getScale() * 0.5f);
+		game_object_1_->transform_.setPosition(mouse_position - game_object_1_->transform_.getScale() * 0.5f);
 	}
 }
 
-void AsteroidsGame::render() {
-	sprite_.render();
-	sprite2_.render();
-}
-
 void AsteroidsGame::destroy() {
-	sprite_.destroy();
-	sprite2_.destroy();
+	sprite_renderer_1->destroy();
+	sprite_renderer_2->destroy();
+
+	game_object_1_->destroy();
+	game_object_2_->destroy();
 }
--- Asteroids/AsteroidsGame.h
+++ Asteroids/AsteroidsGame.h
@@ -1,20 +1,24 @@
 #pragma once
 #include <Engine/Game.h>
 #include <Engine/Camera.h>
-#include <Engine/Sprite.h>
+#include <Engine/GameObject.h>
+#include <Engine/SpriteRenderer.h>
 
 class AsteroidsGame : public Engine::Game {
 
 public:
 	void init() override;
 	void update(const float &delta_time) override;
-	void render() override;
 	void destroy() override;
 
 private:
 	Engine::Camera camera_;
-	Engine::Sprite sprite_;
-	Engine::Sprite sprite2_;
+	Engine::GameObject *game_object_1_;
+	Engine::GameObject *game_object_2_;
+
+	Engine::SpriteRenderer *sprite_renderer_1;
+	Engine::SpriteRenderer *sprite_renderer_2;
+
 	bool drag_mode_ = false;
 
 };
